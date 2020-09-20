de546f7a17291cc050152199a9097e14369b1bba
harish-2306 <snharish.2306@gmail.com>
2020-09-20 21:25:38 +0530

    IDK

--- main.py
+++ main.py
@@ -2,16 +2,16 @@ import numpy as np
 import cv2
 
 from src.border import Horizon
-from src.detect import *
+from src.detect import Detector
 from src.SORT import Sort
 
-sort = Sort()
 
 def video():
     path = "/home/harish/Documents/Okulo Aerosapce/Dataset/Videos/Clip_1.mov"
     cap = cv2.VideoCapture(path)
-    tracker = []
+    detector = Detector()
     horizon = Horizon()
+    sort = Sort()
     
     while True:
         ret, img = cap.read()
@@ -21,21 +21,22 @@ def video():
         img = cv2.resize(img, (700, 400))
         left, right = horizon(img)
         
-        objects = detect(img, left, right)
-        objects, tracker = objects_to_track((400, 700), objects, tracker)
-        objs = np.empty((0, 5))
+        objects = detector(img, left, right)
+        objs = []
         
         for i in objects:
             start_point = ( int(i[0] - 10), int(i[1] -10))
             end_point = (int(i[0] + 10), int(i[1] + 10))
-            x = list(end_point + start_point + (1, ))
-            objs = np.insert(objs, 0, [x])
+            objs.append(list(end_point + start_point + (1, )))
             img = cv2.rectangle(img, start_point, end_point, (0, 0, 255), 1)
-            
+
         if len(objects):
-            print(objs)
+            objs = np.array(objs)
             print(objs.shape)
             print(sort.update(objs))
+            
+        else:
+            print(sort.update(np.empty((0, 5))))
                     
         cv2.line(img, left, right, 255, 2)
 
--- src/SORT.py
+++ src/SORT.py
@@ -197,60 +197,61 @@ def associate_detections_to_trackers(detections,trackers,iou_threshold = 0.3):
 
 
 class Sort(object):
-  def __init__(self, max_age=1, min_hits=3, iou_threshold=0.3):
-    """
-    Sets key parameters for SORT
-    """
-    self.max_age = max_age
-    self.min_hits = min_hits
-    self.iou_threshold = iou_threshold
-    self.trackers = []
-    self.frame_count = 0
+    
+    def __init__(self, max_age=1, min_hits=3, iou_threshold=0.3):
+        """
+        Sets key parameters for SORT
+        """
+        self.max_age = max_age
+        self.min_hits = min_hits
+        self.iou_threshold = iou_threshold
+        self.trackers = []
+        self.frame_count = 0
 
-  def update(self, dets=np.empty((0, 5))):
-    """
-    Params:
-      dets - a numpy array of detections in the format [[x1,y1,x2,y2,score],[x1,y1,x2,y2,score],...]
-    Requires: this method must be called once for each frame even with empty detections (use np.empty((0, 5)) for frames without detections).
-    Returns the a similar array, where the last column is the object ID.
+    def update(self, dets=np.empty((0, 5))):
+        """
+        Params:
+          dets - a numpy array of detections in the format [[x1,y1,x2,y2,score],[x1,y1,x2,y2,score],...]
+        Requires: this method must be called once for each frame even with empty detections (use np.empty((0, 5)) for frames without detections).
+        Returns the a similar array, where the last column is the object ID.
 
-    NOTE: The number of objects returned may differ from the number of detections provided.
-    """
-    self.frame_count += 1
-    # get predicted locations from existing trackers.
-    trks = np.zeros((len(self.trackers), 5))
-    to_del = []
-    ret = []
-    for t, trk in enumerate(trks):
-      pos = self.trackers[t].predict()[0]
-      trk[:] = [pos[0], pos[1], pos[2], pos[3], 0]
-      if np.any(np.isnan(pos)):
-        to_del.append(t)
-    trks = np.ma.compress_rows(np.ma.masked_invalid(trks))
-    for t in reversed(to_del):
-      self.trackers.pop(t)
-    matched, unmatched_dets, unmatched_trks = associate_detections_to_trackers(dets,trks, self.iou_threshold)
+        NOTE: The number of objects returned may differ from the number of detections provided.
+        """
+        self.frame_count += 1
+        # get predicted locations from existing trackers.
+        trks = np.zeros((len(self.trackers), 5))
+        to_del = []
+        ret = []
+        for t, trk in enumerate(trks):
+            pos = self.trackers[t].predict()[0]
+            trk[:] = [pos[0], pos[1], pos[2], pos[3], 0]
+            if np.any(np.isnan(pos)):
+                to_del.append(t)
+        trks = np.ma.compress_rows(np.ma.masked_invalid(trks))
+        for t in reversed(to_del):
+            self.trackers.pop(t)
+        matched, unmatched_dets, unmatched_trks = associate_detections_to_trackers(dets,trks, self.iou_threshold)
 
-    # update matched trackers with assigned detections
-    for m in matched:
-      self.trackers[m[1]].update(dets[m[0], :])
+        # update matched trackers with assigned detections
+        for m in matched:
+            self.trackers[m[1]].update(dets[m[0], :])
 
-    # create and initialise new trackers for unmatched detections
-    for i in unmatched_dets:
-        trk = KalmanBoxTracker(dets[i,:])
-        self.trackers.append(trk)
-    i = len(self.trackers)
-    for trk in reversed(self.trackers):
-        d = trk.get_state()[0]
-        if (trk.time_since_update < 1) and (trk.hit_streak >= self.min_hits or self.frame_count <= self.min_hits):
-          ret.append(np.concatenate((d,[trk.id+1])).reshape(1,-1)) # +1 as MOT benchmark requires positive
-        i -= 1
-        # remove dead tracklet
-        if(trk.time_since_update > self.max_age):
-          self.trackers.pop(i)
-    if(len(ret)>0):
-      return np.concatenate(ret)
-    return np.empty((0,5))
+        # create and initialise new trackers for unmatched detections
+        for i in unmatched_dets:
+            trk = KalmanBoxTracker(dets[i,:])
+            self.trackers.append(trk)
+        i = len(self.trackers)
+        for trk in reversed(self.trackers):
+            d = trk.get_state()[0]
+            if (trk.time_since_update < 1) and (trk.hit_streak >= self.min_hits or self.frame_count <= self.min_hits):
+                ret.append(np.concatenate((d,[trk.id+1])).reshape(1,-1)) # +1 as MOT benchmark requires positive
+            i -= 1
+            # remove dead tracklet
+            if(trk.time_since_update > self.max_age):
+                self.trackers.pop(i)
+        if(len(ret)>0):
+            return np.concatenate(ret)
+        return np.empty((0,5))
 
 def parse_args():
     """Parse input arguments."""
