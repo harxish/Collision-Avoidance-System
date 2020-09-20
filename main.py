5bddb717b0e5d21f69883ed9e06832d29b08edb5
harish-2306 <snharish.2306@gmail.com>
2020-09-20 22:11:12 +0530

    Removed SORT

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ import cv2
 
 from src.border import Horizon
 from src.detect import Detector
-from src.SORT import Sort
 
 
 def video():
@@ -11,7 +10,6 @@ def video():
     cap = cv2.VideoCapture(path)
     detector = Detector()
     horizon = Horizon()
-    sort = Sort()
     
     while True:
         ret, img = cap.read()
@@ -22,21 +20,11 @@ def video():
         left, right = horizon(img)
         
         objects = detector(img, left, right)
-        objs = []
         
         for i in objects:
             start_point = ( int(i[0] - 10), int(i[1] -10))
             end_point = (int(i[0] + 10), int(i[1] + 10))
-            objs.append(list(end_point + start_point + (1, )))
             img = cv2.rectangle(img, start_point, end_point, (0, 0, 255), 1)
-
-        if len(objects):
-            objs = np.array(objs)
-            print(objs.shape)
-            print(sort.update(objs))
-            
-        else:
-            print(sort.update(np.empty((0, 5))))
                     
         cv2.line(img, left, right, 255, 2)
 
